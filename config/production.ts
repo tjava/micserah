44d67651bbbeb53b93be2ba64846b1ca27945f40
tjava <mustyytee@gmail.com>
2022-03-02 02:46:02 +0100

    deploy ready

--- config/production.ts
+++ config/production.ts
@@ -1,5 +1,5 @@
 export default {
-    port: 1333,
+    port: process.env.port || 1333,
     dbUri: "mongodb+srv://E-C_api:Password4E-C_api@cluster0.zdylb.mongodb.net/micserah?retryWrites=true&w=majority",
     saltWorkFactor: 10,
     accessTokenTtl: "15m",
